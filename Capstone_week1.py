1031834da4ae2a728c8c9e836cf1013c4798fead
nikhilsharma55 <44397349+nikhilsharma55@users.noreply.github.com>
2019-11-01 14:10:27 +0530

    Update Capstone_week1.py

--- Capstone_week1.py
+++ Capstone_week1.py
@@ -1,12 +1,5 @@
 
-# coding: utf-8
-
-# In[1]:
-
 #This notebook was created as a requirement of the Applied Data Science Capstone peer graded assignment
-
-
-# In[5]:
 
 import numpy as np
 import pandas as pd
